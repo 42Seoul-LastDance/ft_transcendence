9d6e1bfed56031798220725c90706cf4229b0272
HyunJun <hyunjuki@student.42seoul.kr>
2023-08-22 16:09:00 +0900

    feat : basic GET API with database connection

--- srcs/nest/src/app.module.ts
+++ srcs/nest/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Player } from './player.entity';
+import { PlayersModule } from './players.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgresql',
+      port: 5432,
+      username: 'root',
+      password: 'root1234',
+      database: 'pong_db',
+      entities: [Player], // Your entities here
+      synchronize: false,
+    }),
+    PlayersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
