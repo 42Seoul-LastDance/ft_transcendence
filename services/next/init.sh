885cbb1c298bcb697ef554064792e9816400d217
Jaejun Kim <jaejkim@c4r9s2.42seoul.kr>
2023-09-14 20:46:10 +0900

    chore: eslint prettier add

--- services/next/init.sh
+++ services/next/init.sh
@@ -10,6 +10,8 @@ npm install --prefix srcs next
 # npm install --prefix srcs --save axios-auth-refresh
 # npm install --prefix srcs --save react-router-dom
 # npm install --prefix srcs --save react-cookie
+# npm install --prefix srcs --save jsonwebtoken
+# npm install --prefix srcs --save @typescript-eslint
 
 npm update --prefix srcs react react-dom redux react-redux next @reduxjs/toolkit
 
