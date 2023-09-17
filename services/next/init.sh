fa170d38fd028adb18cd72bc0839423f126f4432
Jaejun Kim <jaejkim@c2r4s2.42seoul.kr>
2023-09-18 00:15:54 +0900

    chore: turbo pack install
     - build time optimize

--- services/next/init.sh
+++ services/next/init.sh
@@ -2,21 +2,14 @@ echo "----- Start installing npm dependencies -----"
 
 npm install --prefix srcs next
 npm install --prefix srcs --save @headlessui/react
-# npm install --prefix srcs --save redux
-# npm install --prefix srcs --save react-redux
-# npm install --prefix srcs --save @reduxjs/toolkit
-# npm install --prefix srcs --save react-unity-webgl
-# npm install --prefix srcs --save nextjs-unity-webgl
-# npm install --prefix srcs --save axios
-# npm install --prefix srcs --save axios-auth-refresh
-# npm install --prefix srcs --save react-router-dom
-# npm install --prefix srcs --save react-cookie
-# npm install --prefix srcs --save jsonwebtoken
+# npm install --prefix srcs --save redux react-redux @reduxjs/toolkit react-devtools
+# npm install --prefix srcs --save react-router-dom react-cookie jsonwebtoken
+# npm install --prefix srcs --save react-unity-webgl nextjs-unity-webgl
+# npm install --prefix srcs --save axios axios-auth-refresh
 # npm install --prefix srcs --save @typescript-eslint
-npm install --prefix srcs --save react-devtools
+# npm install --prefix srcs --save turbo
 
-npm update --prefix srcs react react-dom redux react-redux next @reduxjs/toolkit
-
+npm update --prefix srcs next @headlessui/react redux react-redux @reduxjs/toolkit react-devtools react-router-dom react-cookie jsonwebtoken react-unity-webgl nextjs-unity-webgl axios axios-auth-refresh @typescript-eslint turbo
 echo "----- Successfully installed -----"
 
 npm --prefix srcs run dev --cache-clear
